a7981bbf7cec28dea62170d8b62873399c2250f9
Harkansky <gregoire.wartner@lacatholille.fr>
2025-02-05 11:18:29 +0100

    order dql + fix usercrud

--- esoternet_app/src/Repository/OrderRepository.php
+++ esoternet_app/src/Repository/OrderRepository.php
@@ -16,6 +16,54 @@ class OrderRepository extends ServiceEntityRepository
         parent::__construct($registry, Order::class);
     }
 
+    public function findByDateRange(\DateTime $start, \DateTime $end): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.order_date >= :start')
+            ->andWhere('o.order_date <= :end')
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->orderBy('o.order_date', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findByDate(\DateTime $date): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.order_date = :date')
+            ->setParameter('date', $date)
+            ->orderBy('o.order_date', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findByPriceRange(int $min, int $max): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.total_amount >= :min')
+            ->andWhere('o.total_amount <= :max')
+            ->setParameter('min', $min)
+            ->setParameter('max', $max)
+            ->orderBy('o.total_amount', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findByPrice(int $price): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.total_amount = :price')
+            ->setParameter('price', $price)
+            ->orderBy('o.total_amount', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     //    /**
     //     * @return Order[] Returns an array of Order objects
     //     */
