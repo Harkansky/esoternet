e4e5b4f62ee777099f2a75bd0e51328e77906bdd
Harkansky <gregoire.wartner@lacatholille.fr>
2025-02-09 14:03:53 +0100

    link item creation to dynamic form

--- esoternet_app/src/Controller/Admin/ItemCrudController.php
+++ esoternet_app/src/Controller/Admin/ItemCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Item;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -13,6 +16,14 @@ class ItemCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Item::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->linkToRoute('item_new');
+            });
     }
 
     /*
